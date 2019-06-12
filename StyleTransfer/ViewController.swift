274b55a3a5af4cf20f540cc89a8d38772d29828f
wataruiijima <wataru.iijima@gmail.com>
2019-06-12 19:16:25 +0900

    Initial Commit

---
+++ StyleTransfer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StyleTransfer
+//
+//  Created by w-iijima on 2019/06/12.
+//  Copyright © 2019 w-iijima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
